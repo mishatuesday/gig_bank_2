770ba84be07f72bcfe2cd3fdf64bbf48511367d8
Misha Tuesday <mishatuesday@gmail.com>
2022-12-23 08:47:21 -0500

    add create user

--- client/gig-bank-2/src/Splash.js
+++ client/gig-bank-2/src/Splash.js
@@ -1,10 +1,51 @@
+import { useState } from 'react'
+const userUrl = "http://localhost:3000/users"
 
 function Splash() {
-    return(
+    const [showCreate, setShowCreate] = useState(false)
+    const [userFormData, setUserFormData] = useState([])
+
+    function submitUser() {
+        fetch(userUrl, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(userFormData)
+        })
+        setUserFormData([])
+        setShowCreate(false)
+    }
+
+    return(<>
         <div className="content-panel">
-            Welcome to GigBank! Log in or Sign Up!
+        {showCreate ?
+        <div>
+            <div className="form-title">Create User</div>
+            <label>First Name: </label>
+            <input type="text" name="first_name" id="first_name" value={userFormData.first_name} onChange={(e) => setUserFormData({...userFormData, first_name: e.target.value})} ></input>
+            <label>Last Name: </label>
+            <input type="text" name="last_name" id="last_name" value={userFormData.last_name} onChange={(e) => setUserFormData({...userFormData, last_name: e.target.value})} ></input>
+            <label>Business Name: </label>
+            <input type="text" name="business_name" id="business_name" value={userFormData.business_name} onChange={(e) => setUserFormData({...userFormData, business_name: e.target.value})} ></input>
+            <label>Email: </label>
+            <input type="text" name="email" id="email" value={userFormData.email} onChange={(e) => setUserFormData({...userFormData, email: e.target.value})} ></input>
+            <label>Password: </label>
+            <input type="text" name="password" id="password" value={userFormData.password} onChange={(e) => setUserFormData({...userFormData, password: e.target.value})} ></input>
+            <label>Confirm Password: </label>
+            <input type="text" name="passwd_conf" id="passwd_conf" value={userFormData.passwd_conf} onChange={(e) => setUserFormData({...userFormData, passwd_conf: e.target.value})} ></input>
+            <button onClick={() => submitUser()}>Sign Up!</button>
+            <a href="#" onClick={() => setShowCreate(false)} >cancel</a>
         </div>
-    )
+        
+        :
+        <div>
+            Welcome to GigBank! Log in or <a href="#" onClick={() => setShowCreate(true)} >Sign Up!</a>
+        </div>
+        }
+        </div>
+        </>)
 }
 
 export default Splash
